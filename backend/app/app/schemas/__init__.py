f83f9d954c5848fcf781d586438c12f0ca2abe82
ResTrading <hello@res.trading>
2021-08-21 10:31:33 +0800

    Draft checkout UI and backend

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -5,7 +5,6 @@ from .model import Model, ModelCreate, ModelInDB, ModelUpdate
 from .msg import Msg
 from .product import Product, ProductCreate, ProductInDB, ProductUpdate
 from .category import Category, CategoryCreate, CategoryInDB, CategoryUpdate
-from .model import Model, ModelCreate, ModelInDB, ModelUpdate
 from .order import Order, OrderCreate, OrderInDB, OrderUpdate
 from .token import Nonce, Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
