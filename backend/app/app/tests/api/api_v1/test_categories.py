e8b2c077d57b6f5b82a77541aade0289215ca276
ResTrading <hello@res.trading>
2021-09-16 21:54:11 +0800

    Category unit tests and minor fixes

---
+++ backend/app/app/tests/api/api_v1/test_categories.py
@@ -0,0 +1,55 @@
+from app import crud
+
+from app.tests.utils.utils import random_lower_string
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+
+from app.core.config import settings
+from app.tests.utils.category import create_random_category
+
+
+def test_create_category(
+    client: TestClient, superuser_token_headers: dict, db: Session
+) -> None:
+    name = random_lower_string()
+    data = {
+        'name': name,
+        'slug': name,
+    }
+    response = client.post(
+        f"{settings.API_V1_STR}/categories/", headers=superuser_token_headers, json=data,
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content["name"] == data["name"]
+    assert content["slug"] == data["slug"]
+    assert "id" in content
+
+    crud.category.remove(db, id=content['id'])
+
+
+def test_read_categories(
+    client: TestClient, superuser_token_headers: dict, db: Session
+) -> None:
+    # category = create_random_category(db)
+    response = client.get(
+        f"{settings.API_V1_STR}/categories",
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content
+    # crud.category.remove(db, id=category.id)
+
+
+def test_read_category(
+    client: TestClient, superuser_token_headers: dict, db: Session
+) -> None:
+    category = create_random_category(db)
+    response = client.get(
+        f"{settings.API_V1_STR}/categories/{category.id}",
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content["id"] == category.id
+    assert content["name"] == category.name
+    crud.category.remove(db, id=category.id)
---
+++ backend/app/app/tests/utils/category.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from sqlalchemy.orm import Session
+
+from app import crud, models
+from app.schemas import CategoryCreate
+from app.tests.utils.utils import random_lower_string
+
+
+def create_random_category(
+    db: Session
+) -> models.Category:
+    name = random_lower_string()
+    new_category = CategoryCreate(name=name, slug=name)
+    category = crud.category.create(db, obj_in=new_category)
+
+    return category
