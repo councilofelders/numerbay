55f18082b217deb2cc59405ae6949041e9c813d6
ResTrading <hello@res.trading>
2021-10-29 16:31:10 +0800

    Lint

--- backend/app/app/db/init_db.py
+++ backend/app/app/db/init_db.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import Session
 from app import crud, schemas
 from app.core.config import settings
 from app.db import base  # noqa: F401
-
 # make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
 # otherwise, SQL Alchemy might fail to initialize relationships properly
 # for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
