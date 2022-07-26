a9ae28434eddc67117ca3156b4f0e5d9cbcac7e5
ResTrading <hello@res.trading>
2022-07-26 09:40:34 +0800

    Update QE dashboard link, closes #251

--- frontend/packages/theme/pages/Home.vue
+++ frontend/packages/theme/pages/Home.vue
@@ -158,7 +158,7 @@ export default {
           title: 'Power BI Numerai Dashboard',
           desc: 'Numerai leaderboard, round performances, payouts and more',
           author: '@QE',
-          link: 'https://app.powerbi.com/view?r=eyJrIjoiYTI4N2FkY2YtYTliYy00ZjQ5LWE2NjMtMTBjOTkwYTg2NDE2IiwidCI6Ijg3ZDc2ZDQ2LWYxZmYtNDkzMi05MGNiLTUyNzY3Yzg2OTk2ZiIsImMiOjl9'
+          link: 'https://qe-intelligence.com/numerai-statistics/'
         },
         {
           id: 3,
