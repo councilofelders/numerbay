81f9311eed51b2cde49ed6de9b660c2baa02f129
ResTrading <hello@res.trading>
2024-12-22 22:50:05 +0800

    Update scripts to docker v2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ DOMAIN=${DOMAIN?Variable not set} \
 TRAEFIK_TAG=${TRAEFIK_TAG?Variable not set} \
 STACK_NAME=${STACK_NAME?Variable not set} \
 TAG=${TAG?Variable not set} \
-docker-compose \
+docker compose \
 -f docker-compose.yml \
 config > docker-stack.yml
 
