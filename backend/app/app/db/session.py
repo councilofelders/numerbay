56a068e86265789b3215902147c81320c1db85a6
ResTrading <hello@res.trading>
2025-03-30 14:43:11 +0800

    Increase db pool

--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -3,5 +3,11 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URI,
+    pool_size=15,
+    max_overflow=20,
+    pool_timeout=60,
+    pool_pre_ping=True,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
